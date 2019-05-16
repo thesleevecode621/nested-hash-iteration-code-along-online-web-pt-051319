bc97c65568a5b76c5d2b011441d10ccd40f05311
boruch d <dubywider@gmail.com>
2019-05-16 21:42:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,9 +10,4 @@ def remove_strawberry(contacts)
 end  
 
   
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-    binding.pry
-  end
-end
+
