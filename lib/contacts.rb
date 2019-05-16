00015f6268757a58edb4d416d0126c4dfa84ea26
boruch d <dubywider@gmail.com>
2019-05-16 21:40:54 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,18 +1,7 @@
 require 'pry'
 
   
- contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-    },
-   "Freddy Mercury" => {
-       name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
- }
+
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
@@ -22,4 +11,4 @@ def remove_strawberry(contacts)
 
 end
 
-end
+
